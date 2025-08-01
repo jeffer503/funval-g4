2af825d515f9bdaf26c8b0a20674a89bf82a1160
jeffer503 <jeffersonhomero123@gmail.com>
2025-07-31 19:48:53 -0600

    practica 10

--- practica-10/main.js
+++ practica-10/main.js
@@ -5,10 +5,21 @@ async function obtenerDatos() {
       throw new Error('Error en la respuesta');
     }
     const datos = await respuesta.json();
-    console.log(datos);
+    const contenedor = document.querySelector("#contenedor");
+    datos.forEach(usuario => {
+      contenedor.innerHTML += `<div class="max-w-sm mx-auto bg-white shadow-md rounded-lg p-6 border border-gray-200">
+        <h3 class="text-xl font-semibold mb-2">Nombre: ${usuario.name} Doe</h3>
+        <p class="text-gray-700"><span class="font-medium">Usuario:</span>${usuario.username}</p>
+        <p class="text-gray-700"><span class="font-medium">Email:</span> ${usuario.email}</p>
+        <p class="text-gray-700"><span class="font-medium">Empresa:</span> ${usuario.company.name}</p>
+    </div>`
+    });
+
+    
   } catch (error) {
-    console.error('Error al conectar con la API:', error);
+    console.log("Error al conectar con la API");
   }
 }
 
 obtenerDatos();
+
